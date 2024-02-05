9b27f0dfdc5b393366028609d6a87de5f512450c
imran2046 <md.imranali2046@gmail.com>
2024-02-05 12:10:03 +0600

    JS

---
+++ Js Newbie Projects/Inch to Feet.js
@@ -0,0 +1,10 @@
+function inchTofeet(inch){
+var feet = inch/12;
+return feet;
+}
+var ezio = inchTofeet(160);
+console.log(ezio);
+var altair = inchTofeet(162);
+console.log(altair);
+var edward = inchTofeet(156);
+console.log(edward);
